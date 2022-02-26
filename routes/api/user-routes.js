3c5d14cb31d92a8d691dc63a65742f2743236315
Daniel Kim <daniel.d.kim13@gmail.com>
2022-02-26 02:59:52 -0500

    updated vote

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -19,6 +19,18 @@ router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
     where: { id: req.params.id },
+    include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
+        }
+      ]
   })
     .then((dbUserData) => {
       if (!dbUserData) {
