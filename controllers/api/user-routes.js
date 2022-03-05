a1dd8f1bc802811e4dd6286660fdb48f8a552a3d
Daniel Kim <daniel.d.kim13@gmail.com>
2022-03-05 08:28:24 -0500

    lesson 2

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
-// GET /api/users.
+// get all users
 router.get("/", (req, res) => {
-  // access our user model and run findall method.
   User.findAll({
     attributes: { exclude: ["password"] },
   })
@@ -14,23 +13,32 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/users/1.
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
-    where: { id: req.params.id },
+    where: {
+      id: req.params.id,
+    },
     include: [
-        {
+      {
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
           model: Post,
-          attributes: ['id', 'title', 'post_url', 'created_at']
+          attributes: ["title"],
         },
-        {
-          model: Post,
-          attributes: ['title'],
-          through: Vote,
-          as: 'voted_posts'
-        }
-      ]
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        through: Vote,
+        as: "voted_posts",
+      },
+    ],
   })
     .then((dbUserData) => {
       if (!dbUserData) {
@@ -45,14 +53,22 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// POST /api/users.
 router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -60,7 +76,7 @@ router.post("/", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // Query operation
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
@@ -71,26 +87,45 @@ router.post("/login", (req, res) => {
       return;
     }
 
-    // res.json({user:dbUserData});
-
-    // Verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: "Incorrect password!" });
       return;
     }
-    res.json({ user: dbUserData, message: "You are now logged in!" });
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
   });
 });
 
-// PUT api/users/1.
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 router.put("/:id", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
-    where: { id: req.params.id },
+    where: {
+      id: req.params.id,
+    },
   })
     .then((dbUserData) => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: "No user found with this id" });
         return;
       }
@@ -102,7 +137,6 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// DELETE /api/users/1.
 router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
